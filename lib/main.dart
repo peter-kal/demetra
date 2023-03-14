ff00251809dd3ded5c9aa37114f94127c0a4c989
Peter Kal <koiliaris.peter@gmail.com~>
2023-03-14 20:06:42 +0200

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:demetra/view/home_page.dart';
+import 'package:yaru/yaru.dart' as yaru;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demetra',
+      theme: yaru.yaruLight,
+      darkTheme: yaru.yaruDark,
+      home: HomePage(),
+    );
+  }
+}
