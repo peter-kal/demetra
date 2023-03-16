afed8122445be05bb8c05be980f25ca73a1314b5
Peter Kal <koiliaris.peter@gmail.com~>
2023-03-16 12:28:43 +0200

    Added maximum lenght and min-man lines on Textfields

--- lib/view/dialog_task-creation.dart
+++ lib/view/dialog_task-creation.dart
@@ -9,42 +9,71 @@ class DialogPage extends StatefulWidget {
 }
 
 class _DialogPageState extends State<DialogPage> {
-  
+  late TextEditingController _controller;
+  late TextEditingController _controller2;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    _controller2 = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _controller2.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-  return  Center(
-              child: ElevatedButton(
-                  onPressed: () => showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      
+    return Center(
+      child: ElevatedButton(
+          onPressed: () => showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
                       titlePadding: EdgeInsets.zero,
-                      title: YaruDialogTitleBar(
-                        leading: const Center(
-                          child: SizedBox.square(
-                            dimension: 40,
-                        
+                      title: const YaruDialogTitleBar(
+                        leading: Center(
+                          child: SizedBox(
+                            width: 300,
+                            height: 100,
                           ),
                         ),
-                        title: const Text('Create A Task'),
-                        
+                        title: Text('Create A Task'),
                       ),
-                      content: SizedBox(
-                        height: 100,
-                        child: YaruBanner.tile(
-                          surfaceTintColor: Colors.pink,
-                          title: Text(
-                            'he'
-                                                   ),
-                       
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                  child: const Text('+ Add a task')),
-            );
-   
-   }
-   }
+                      content: Center(
+                          child: Column(
+                        children: [
+                          const Text('Name:'),
+                          TextField(
+                            decoration: const InputDecoration(
+                              labelText: 'Name',
+                            ),
+                            maxLength: 50,
+                            maxLines: 2,
+                            minLines: 1,
+                            controller: _controller,
+                            onSubmitted: ((title) => String),
+                          ),
+                          const Text('Details:'),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Description',
+                            ),
+                            maxLines: 8,
+                            minLines: 1,
+                            maxLength: 200,
+                            controller: _controller2,
+                            onSubmitted: ((description) => String),
+                          ),
+                        ],
+                      )));
+                },
+              ),
+          child: const Text('+ Add a task')),
+    );
+  }
+}
